d4d944629d6cfb4128fa914857919033b5bb414d
Sajeev Magesh <sajeevmagesh123@gmail.com>
2023-08-29 01:01:52 -0700

    new push removing unneccessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Logo from './assets/logocopy2.png';
 import LogoNoText from './assets/logocopy.png';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
